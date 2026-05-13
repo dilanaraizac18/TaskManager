a3f1f850c876a6662561fe866804e80388b83989
dilanaraizac18 <dilanaraiza18@outlook.com>
2026-05-13 15:43:58 -0600

    feat: Creación del package Repository

---
+++ src/main/java/TaskManager/YaGanaste/Entity/Repository/ITask.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package TaskManager.YaGanaste.Entity.Repository;
+
+/**
+ *
+ * @author digis
+ */
+public interface ITask {
+    
+}
