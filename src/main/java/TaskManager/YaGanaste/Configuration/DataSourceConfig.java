b170e50f563f5f5770c8d04ebafcdc8b4f9bffb2
dilanaraizac18 <dilanaraiza18@outlook.com>
2026-05-13 15:23:34 -0600

    feat: Configuración para el mapeo de objetos y acceso a base de datos

---
+++ src/main/java/TaskManager/YaGanaste/Configuration/DataSourceConfig.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TaskManager.YaGanaste.Configuration;
+
+import javax.sql.DataSource;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+/**
+ *
+ * @author digis
+ */
+@Configuration
+public class DataSourceConfig {
+    
+    
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:orcl");
+        dataSource.setUsername("DAraizaProgramacionNCapas");
+        dataSource.setPassword("password1");
+        
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource){
+        return new JdbcTemplate(dataSource);
+    }
+    
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+    
+}
